0d477737afcb391d90553c97b6821b614ecb4d7f
almoggr <34807230+almoggr@users.noreply.github.com>
2024-01-14 11:52:29 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,80 @@
+import React , { useState} from 'react'
+
+function Calculator() {
+
+    const [calc, setCalc] = useState('')
+    const [ result, setResult ] = useState('')
+    const [ finalResult , setFinalResult ] = useState('')
+
+    const ops = [ '/' , '*' , '+' , '-' , '.']
+    const updateCalc = value => {
+        if( ops.includes(value) && calc === '' || ops.includes(value) && ops.includes(calc.slice(-1))){
+            return ;
+        }
+        setCalc(calc + value)
+
+        if( !ops.includes(value) ){
+            setResult(eval(calc + value).toString())
+        }
+    }
+  
+    const createDigits = () => {
+        const digits = []
+        for(let i = 1; i < 10; i++){
+            digits.push(
+            <button 
+                onClick={ () => updateCalc(i.toString()) }
+                key={i}>
+                {i}
+            </button>)
+        }
+        return digits;
+    }
+
+    const calculate = () => {
+        setCalc(eval(calc.toString()))
+    }
+
+    const deleteLast = () => {
+        console.log(calc)
+        console.log(result)
+        if( calc == '') {
+            return;
+        }
+        if ( calc != result ){
+            const value = calc.slice( 0 , -1);
+            setCalc(value)
+        }else {
+            setCalc('')
+            setResult('')
+        }
+    }
+  
+    return (
+    <div className='container'>
+        <h1 className='title'>React Calculator</h1>
+        <div className='calculator'>
+            <div className='display'>
+                {/* { finalResult ? finalResult : (calc != '' ? <div> <span> ({ result }) </span> { calc } </div> : '0') } */}
+              { result ? <span> ({ result }) </span> : ''} { calc || '0'}
+            </div>
+            <div className='operators'>
+                <button onClick={ () => updateCalc('*')}>*</button>
+                <button onClick={ () => updateCalc('/')}>/</button>
+                <button onClick={ () => updateCalc('+')}>+</button>
+                <button onClick={ () => updateCalc('-')}>-</button>
+
+                <button onClick= { deleteLast }>DEL</button>
+              </div>
+              <div className='digits'>
+                { createDigits() }
+                <button onClick={ () => updateCalc('0')}>0</button>
+                <button onClick={ () => updateCalc('.')}>.</button>
+                <button onClick={ calculate }>=</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Calculator;
